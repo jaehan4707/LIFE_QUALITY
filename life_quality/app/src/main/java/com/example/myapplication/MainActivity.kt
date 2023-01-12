0199d92673fba2b56f392bf535dd8ef5c878601e
KCM <kcm4112@naver.com>
2023-01-12 20:54:27 +0900

    layout coding

--- life_quality/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ life_quality/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.qStart.setOnClickListener() {
+            val intent = Intent(this, QuestionMainpage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
