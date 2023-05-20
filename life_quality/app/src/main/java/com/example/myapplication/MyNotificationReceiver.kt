86eb0a0f9011f431f71a21fae3d139010ef04c3c
jaehan4707 <jahan4707@naver.com>
2023-05-21 01:31:33 +0900

    알림에 진동 넣고, 알림 클릭시 앱 시작화면 수정

--- life_quality/app/src/main/java/com/example/myapplication/MyNotificationReceiver.kt
+++ life_quality/app/src/main/java/com/example/myapplication/MyNotificationReceiver.kt
@@ -25,7 +25,6 @@ class MyNotificationReceiver : BroadcastReceiver() {
         //푸시 알림을 보내기위해 시스템에 권한을 요청하여 생성
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         //안드로이드 오레오 버전 대응
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             manager.createNotificationChannel(
                 NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
@@ -35,16 +34,14 @@ class MyNotificationReceiver : BroadcastReceiver() {
             builder = NotificationCompat.Builder(context)
         }
         //알림창 클릭 시 지정된 activity 화면으로 이동
-        val intent2 = Intent(context, SplashActivity::class.java)
-
+        val alarmIntent = Intent(context, SplashActivity::class.java)
         // FLAG_UPDATE_CURRENT ->
         // 설명된 PendingIntent가 이미 존재하는 경우 유지하되, 추가 데이터를 이 새 Intent에 있는 것으로 대체함을 나타내는 플래그입니다.
         // getActivity, getBroadcast 및 getService와 함께 사용
-        //val pendingIntent = PendingIntent.getActivity(context, 101, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
-        val pendingIntent = PendingIntent.getBroadcast(
+        val pendingIntent = PendingIntent.getActivity(
             context,
             0,
-            intent2,
+            alarmIntent,
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
             } else {
@@ -53,15 +50,12 @@ class MyNotificationReceiver : BroadcastReceiver() {
         )
         //알림창 제목
         builder.setContentTitle(contentValue) //회의명노출
-        //builder.setContentText(intent.getStringExtra("content")); //회의 내용
-        //알림창 아이콘
-        builder.setSmallIcon(R.mipmap.ic_launcher_android)
-        //알림창 터치시 자동 삭제
-        builder.setAutoCancel(true)
-        builder.setContentIntent(pendingIntent)
-        //푸시알림 빌드
-        val notification = builder.build()
-        //NotificationManager를 이용하여 푸시 알림 보내기
-        manager.notify(1, notification)
+        builder.setSmallIcon(R.mipmap.ic_launcher_android) //알림창 아이콘
+        builder.setAutoCancel(true) //알림창 터치시 자동 삭제
+        builder.setContentIntent(pendingIntent) //알림 터치시 pending intent로
+        builder.setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+
+        val notification = builder.build()//푸시알림 빌드
+        manager.notify(1, notification) //NotificationManager를 이용하여 푸시 알림 보내기
     }
 }
