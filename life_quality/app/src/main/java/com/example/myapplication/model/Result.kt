ddf0498ffa7b7a59313017db4e1b8eae8c42cefb
jaehan4707 <jahan4707@naver.com>
2023-06-24 21:29:05 +0900

    디자인 수정

---
+++ life_quality/app/src/main/java/com/example/myapplication/model/Result.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.model
+
+class Result (
+    val mna : String,
+    val sgdsk : String,
+    val yosil : String,
+    val mouth : String,
+    val ipaq : String,
+    val sleep : String,
+    val frailty : String,
+    val fall : String,
+    val eq5d : String
+)
